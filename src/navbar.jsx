f8e4cdd5723e670b114bbdbc71ae12e293462342
prathamhanda <prathamhanda10@gmail.com>
2025-03-02 22:32:28 +0530

    Navbar made non-sticky

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
 
   return (
-    <nav className="shadow-lg bg-transparent font-montserrat sticky top-0 z-50">
+    <nav className="shadow-lg bg-transparent font-montserrat z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
@@ -20,7 +20,7 @@ const Navbar = () => {
           </div>
           
           {/* Desktop Menu */}
-          <div className="hidden md:flex space-x-4">
+          <div className="hidden md:flex space-x-4 overflow-x-auto">
             <Link to="/" className="text-gray-700 hover:text-[#FE6F61] px-3 py-2 rounded-md font-medium">
               Home
             </Link>
