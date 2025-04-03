c2c8f774c92b69c11cba91ac37109f5202e28ed6
Your Name <your.email@example.com>
2025-04-03 12:55:34 +0530

    Updated the ports for DNS

--- frontend_landlord/vite.config.js
+++ frontend_landlord/vite.config.js
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: 'roomsonrent.in', // Set the host
+    port: 80, // Change port if needed
+    strictPort: true, // Ensure it fails if the port is not available
+    cors: true, // Enable CORS if required
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`, // Example: Auto import global styles
+      },
     },
   },
 })
